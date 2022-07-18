a846a3faf4d361bc5505d1aaf4c623cf1718bb9a
Kaze Wong <kazewong.physics@gmail.com>
2022-07-18 12:22:50 -0400

    Add lightning simclr

---
+++ lightning/models/SIMCLR.py
@@ -0,0 +1,16 @@
+from model.SimCLR import SimCLR, SimCLRAugmentation, NT_Xent
+import pytorch_lightning as pl
+
+class SIMCLR_lightning(pl.LightningModule):
+
+    def __init__(self):
+        pass
+
+    def training_step(self, batch, batch_idx):
+        pass
+
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def test_step(self, batch, batch_idx):
+        pass
